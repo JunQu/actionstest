14cf8f4e075e6392817de932422caf8fe8002c46
JunQu <junjv@outlook.com>
2020-06-23 16:00:59 +0000

    Deploying to gh-pages from  @ f13997637dd072cff6a61912c9255ae65d981812 🚀

---
+++ precache-manifest.5fa4a23f87950f367d84bfdb00471155.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe219c231a6777fe2635d98397ca18da",
+    "url": "/actionstest/index.html"
+  },
+  {
+    "revision": "9bc88683f3254778a32b",
+    "url": "/actionstest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/actionstest/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/actionstest/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc88683f3254778a32b",
+    "url": "/actionstest/static/js/main.ac24c38b.chunk.js"
+  },
+  {
+    "revision": "0e51371352ef8d6eb678",
+    "url": "/actionstest/static/js/runtime-main.9ac44f0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/actionstest/static/media/logo.5d5d9eef.svg"
+  }
+]);
